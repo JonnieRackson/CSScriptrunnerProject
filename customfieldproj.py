1a410fbcd465383fcee2e3acd9ca82f371807cce
JonnieRackson <rjackson784@gmail.com>
2023-10-10 18:45:34 -0500

    Update customfieldproj.py

--- customfieldproj.py
+++ customfieldproj.py
@@ -6,49 +6,51 @@ url = "https://your-domain.atlassian.net/rest/api/2/field/search"
 
 auth = HTTPBasicAuth("email@example.com", "<api_token>")
 
+headers = {
+    "Accept": "application/json"
+}
+
 # Define the custom field ID
 custom_field_id = "customfield_XXXXX"  # Replace XXXXX with your custom field ID
 
 # Get a list of all projects
-response = requests.get(f"{url}/project", headers={"Authorization": f"Basic {api_token}"})
+projects_url = "https://your-jira-instance/rest/api/2/project"
+projects_response = requests.request(
+    "GET",
+    projects_url,
+    headers=headers,
+    auth=auth
+)
 
-if response.status_code == 200:
-    projects = response.json()
+if projects_response.status_code == 200:
+    projects = projects_response.json()
+    total_count = 0
     
-    # Initialize a counter for field usage
-    total_usage_count = 0
-    
-    # Initialize a dictionary to store project usage counts
-    project_usage_counts = {}
-    
-    # For each project, count how many times the custom field has been used
     for project in projects:
         project_key = project['key']
-        
-        # Define the JQL query to search for issues with the custom field in the specific project
-        jql_query = f'cf[{custom_field_id}] is not EMPTY AND project = {project_key}'
+        jql_query = f'project = {project_key} AND cf["{custom_field_id}"] is not EMPTY'
         
         # Define parameters for the API request
         params = {
             "jql": jql_query,
             "maxResults": 1000  # Adjust as needed
         }
-        
+
         # Make the API request to search for issues
-        response = requests.get(f"{url}/search", params=params, headers={"Authorization": f"Basic {api_token}"})
-        
-        if response.status_code == 200:
-            issues = response.json()['issues']
-            field_usage_count = len(issues)
-            total_usage_count += field_usage_count
-            project_usage_counts[project_key] = field_usage_count
+        project_response = requests.request(
+            "GET",
+            url,
+            headers=headers,
+            auth=auth,
+            params=params  # Include the params in the request
+        )
+
+        if project_response.status_code == 200:
+            issues = project_response.json()['issues']
+            total_count += len(issues)
         else:
-            print(f"Failed to retrieve issue data for project {project_key}. Status code: {response.status_code}")
+            print(f"Failed to retrieve issue data for project {project_key}. Status code: {project_response.status_code}")
     
-    # Generate a report
-    print(f"\nTotal field usage across all projects: {total_usage_count}\n")
-    print("Project Usage Report:")
-    for project_key, usage_count in project_usage_counts.items():
-        print(f"Project {project_key}: {usage_count} usage(s)")
+    print(f"The custom field with ID {custom_field_id} has been used in {total_count} issues across all projects.")
 else:
-    print(f"Failed to retrieve project data. Status code: {response.status_code}")
+    print(f"Failed to retrieve project data. Status code: {projects_response.status_code}")
