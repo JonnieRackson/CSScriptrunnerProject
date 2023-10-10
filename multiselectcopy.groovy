7fa69c9e2a3701b21e764b0925c8a6d30e3f1a2a
JonnieRackson <rjackson784@gmail.com>
2023-10-10 16:07:21 -0500

    Add files via upload

---
+++ multiselectcopy.groovy
@@ -0,0 +1,28 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.issue.CustomFieldManager
+import com.atlassian.jira.issue.Issue
+
+// Define the custom field IDs
+def sourceFieldName = "customfield_10001" // Replace with your source custom field ID
+def targetFieldName = "customfield_10002" // Replace with your target custom field ID
+
+// Get the custom field manager
+def customFieldManager = ComponentAccessor.getCustomFieldManager()
+
+// Get the source and target custom fields
+def sourceField = customFieldManager.getCustomFieldObject(sourceFieldId)
+def targetField = customFieldManager.getCustomFieldObject(targetFieldId)
+
+// Get all issues in the project 
+def issueManager = ComponentAccessor.getIssueManager()
+def issues = ComponentAccessor.getIssueManager().getIssueObjects()
+
+// Iterate through the issues and copy values
+issues.each { issue ->
+    def sourceValues = issue.getCustomFieldValue(sourceField)
+
+    if (sourceValues) {
+        issue.setCustomFieldValue(targetField, sourceValues)
+        issueManager.updateIssue(user, issue, com.atlassian.jira.event.type.EventDispatchOption.DO_NOT_DISPATCH, false)
+    }
+}
